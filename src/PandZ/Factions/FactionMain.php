a4eea6cd541ebeca7cd0c029edd772042c976eb9
Pizzahaet <graysteven09.sg@gmail.com>
2017-04-23 19:27:47 -0400

    Slight revert
    No need for for Listener here

---
+++ src/PandZ/Factions/FactionMain.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FactionsPZ;
+
+/* CODED BY @ZorexDesigns and @PizzaHaet */
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+//Better to make an EventListener.php file so we don't clutter FactionMain.php or so I was told by Thunder
+//use pocketmine\event\Listener;
+
+ class FactionMain extends PluginBase {
+ 
+ 
+ }
+//?>
+//Is that even needed? I mean just look at many other plugins you rarely see it... I believe it's used in html with the script tag
