5fd402b13ea634e2566dea8526af87d649cbc84e
Pizzahaet <graysteven09.sg@gmail.com>
2017-04-23 20:38:07 -0400

    I is dumbz

--- src/Factions/EventListener.php
+++ src/Factions/EventListener.php
@@ -6,8 +6,7 @@ use pocketmine\event\Listener;
 use Factions\FactionMain;
 
 class EventListener implements Listener{
-/*Do I extend FactionMain? Like so...
-* class EventListener implements Listener extends FactionMain{
-*/
+ 
+
 
 }
