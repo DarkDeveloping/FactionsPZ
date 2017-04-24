c070be616f7defece5d44d8091098cb2899d5cc5
Pizzahaet <graysteven09.sg@gmail.com>
2017-04-23 20:24:16 -0400

    ?

---
+++ src/Factions/EventListener.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Factions\FactionsMain;
+
+use pocketmine\event\Listener;
+use Factions\FactionMain;
+
+class EventListener implements Listener{
+/*Do I extend FactionMain? Like so...
+* class EventListener implements Listener extends FactionMain{
+*/
+
+}
